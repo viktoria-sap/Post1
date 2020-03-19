23475020e3b419162fb7d508cd2dded50d3b925a
vika <v.sapunkova@neurobotics.ru>
2020-03-20 00:25:36 +0300

    PostVkManager

---
+++ src/main/java/ru/netology/manager/PostVkManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.PostVk;
+
+public class PostVkManager {
+    private PostVk[] objects;
+
+    public PostVk[] search(ownerId, domain, query, ownersOnly, count, offset) {
+        // TODO: add logic
+        return null;
+    }
+
+    public void delete(ownerId, postId){
+
+    }
+}
